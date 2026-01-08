80977b120c27361245a1611e797588c55ef877f1
Jack Zhuang <9340100@qq.com>
2026-01-09 03:57:10 +0800

    Add Data Explorer to dashboard and update server config
    Introduces a Data Explorer component to the dashboard for browsing and viewing database objects and records. Updates static file serving in app.module.ts to prevent fallback to index.html for non-UI routes. Sets 'rootDir' in tsconfig.json to './src' for improved TypeScript project structure.

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -14,6 +14,7 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../../ui/dist'),
       serveRoot: '/assets/ui',
+      renderPath: '/assets/ui/*' // Prevent fallback to index.html for api or other routes
     }),
   ],
   controllers: [AppController, ViewsController],
