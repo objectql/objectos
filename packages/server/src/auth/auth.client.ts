87e254d899377428a91f8b06a78e53efa9387bbf
Jack Zhuang <9340100@qq.com>
2026-01-09 17:21:42 +0800

    Update auth.client.ts

--- packages/server/src/auth/auth.client.ts
+++ packages/server/src/auth/auth.client.ts
@@ -14,7 +14,7 @@ export const getAuth = async () => {
         });
         authInstance = betterAuth({
             database: pool,
-            trustedOrigins: ["http://localhost:5173"],
+            trustedOrigins: ["http://localhost:5173", "http://localhost:3000"],
             emailAndPassword: {
                 enabled: true
             },
